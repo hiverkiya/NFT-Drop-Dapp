20afe10ed1d3de52419303579d1f6298a656d0cc
hiverkiya <hiverkiya@gmail.com>
2022-10-09 18:08:49 +0530

    initialBuild

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 function NFTDropPage() {
+  // Auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10 ">
       {/* Left*/}
@@ -38,11 +43,26 @@ function NFTDropPage() {
             </span>{" "}
             NFT Market Place
           </h1>
-          <button className="uppercase rounded-full bg-rose-500 px-4 text-xs text-white font-bold py-2 lg:px-5 lg:py-3 lg:text-base ">
-            Sign In
+          <button
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className="uppercase rounded-full bg-rose-500 px-4 text-xs text-white font-bold py-2 lg:px-5 lg:py-3 lg:text-base "
+          >
+            {address ? "Sign Out" : "Sign In"}
           </button>
         </header>
         <hr className="my-2 border" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            {" "}
+            You're logged in with{" "}
+            <span className="uppercase font-bold">
+              {address.substring(0, 5)}...
+              {address.substring(address.length - 5)}
+            </span>{" "}
+            wallet
+          </p>
+        )}
+
         {/*Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
           <img
@@ -60,7 +80,10 @@ function NFTDropPage() {
           </p>
         </div>
         {/*Mint Button */}
-        <button className="h-16 bg-rose-500 font-bold text-white rounded-full"> MINT NFT (0.01 ETH)</button>
+        <button className="h-16 bg-rose-500 font-bold text-white rounded-full">
+          {" "}
+          MINT NFT (0.01 ETH)
+        </button>
       </div>
     </div>
   );
