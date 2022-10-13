c7f2752ff8f262e7aba0f25c9b0ed9ec7affbfde
hiverkiya <hiverkiya@gmail.com>
2022-10-13 13:02:14 +0530

    Design changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ interface Props {
 }
 const Home = ({ collections }: Props) => {
   return (
-    <div className="max-w-7xl mx-auto flex flex-col min-h-screen py-20 px-10 2xl:px-0">
+    <div className="max-w-full mx-auto flex flex-col min-h-screen py-20 px-10 2xl:px-0 bg-gradient-to-br from-lime-300 to-red-300 rounded-xl">
       <Head>
         <title>NFT Drop</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/nft.png" />
       </Head>
-      <h1 className="uppercase mb-10 text-4xl font-extralight">
+      <h1 className="uppercase mb-10 text-4xl font-extralight text-center">
         {" "}
         The{" "}
         <span className="font-extrabold underline decoration-rose-500/60">
@@ -22,7 +22,7 @@ const Home = ({ collections }: Props) => {
         </span>{" "}
         NFT MarketPlace
       </h1>
-      <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
+      <main className="bg-gradient-to-br from-lime-200 to-red-200 p-10 shadow-xl shadow-rose-400/20">
         <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {collections.map((collection) => (
             <Link href={`/nft/${collection.slug.current}`}>
@@ -33,7 +33,7 @@ const Home = ({ collections }: Props) => {
                 />
                 <div className="p-5">
                   <h2 className="text-3xl">{collection.title}</h2>
-                  <p className="mt-2 text-sm text-gray-400">
+                  <p className="mt-2 text-l text-black-400">
                     {collection.description}
                   </p>
                 </div>
